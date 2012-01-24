23cd4f490f5c55b487365e602d1bdd7cb0145a5b
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2012-01-24 02:58:06 +0100

    + debian package creation script, using checkinstall

---
+++ create-debian-package.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+MAINTAINER='Nils Dagsson Moskopp \<nils@dieweltistgarnichtso.net\>'
+SUMMARY='Terminal-based frontend for the minimalist filesystem-based IRC client ii.'
+DEPENDENCIES='busybox \| busybox-static, ii, inotify-tools, lsof, coreutils, procps, rlwrap, sed, dash, splitvt, ncurses-bin, libnotify-bin'
+
+checkinstall --fstrans --install=no --reset-uids --deldoc \
+    --maintainer "$MAINTAINER" \
+    --pkgname "uii" \
+    --pkglicense "GPL3" \
+    --pkggroup "net" \
+    --pkgarch "all" \
+    --pkgsource "https://github.com/erlehmann/uii" \
+    --requires "$DEPENDENCIES" \
+    ./install.sh
